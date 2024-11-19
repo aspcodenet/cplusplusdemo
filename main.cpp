98a2e007b6b051b8afe137f7718eed948b9ff5c6
Stefan Holmberg <stefan.holmberg@systementor.se>
2024-11-19 10:11:00 +0100

    v2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
 
+using namespace std;
+int main2(){
+    string namn = "Stefan";
+    namn += " Holmberg";
+
+    int age;
+    
+
+    cout << "Tjena" << endl;
+    //"Hej Stefan du är bara 52 dvs evigt ung"
+    cout << "Hej " << namn << " du är bara " << age <<  " years dvs evigt ung";
+    return 0;
+}
+
+// string compare
+// string!
+// DEMO class + vector
+
+
 int main(){
+    std::string namn ;
+    int age ;
+  
+    std::cout << "Vad heter du?";
+    std::cin >> namn;
+    std::cout << "Hur gammal är du?";
+    std::cin >> age;
+  
+
+
+    //std::cout << "Tjena" << std::endl;
+    //"Hej Stefan du är bara 52 dvs evigt ung"
+    std::cout << "Hej " << namn << " du är bara " << age <<  " år dvs evigt ung";
     return 0;
 }
